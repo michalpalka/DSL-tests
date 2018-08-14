30f920820e1d5d9e5c3955705545115ef2c5ecc5
Michał Pałka <michal.palka@chalmers.se>
2018-08-14 13:08:07 +0200

    Working csv parsing

--- src/DSLDTest.hs
+++ src/DSLDTest.hs
@@ -49,4 +49,4 @@ genMain = do
 
 genMainProp = do
   withFile "test3.c" WriteMode $ \h -> do
-    hPutDocLn h $ ppr $ mainReadCSVProp [(1, tExpToCBlock test1)]
+    hPutDocLn h $ ppr $ mainReadCSVProp [(0, tExpToCBlock "res" test1)] ["col1"]
